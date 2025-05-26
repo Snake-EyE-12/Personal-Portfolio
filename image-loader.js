01f32a7b0917805b7110404974e8ef09dda7edf9
Chase Guymon <chase.guymon@q.com>
2025-05-25 19:23:36 -0600

    Configuration

--- image-loader.js
+++
@@ -1,4 +0,0 @@
-export default function imageLoader({ src, width, quality }) {
-  const prefix = process.env.NODE_ENV === 'production' ? '/Portfolio' : ''
-  return `${prefix}${src}?w=${width}&q=${quality || 75}`
-} 
